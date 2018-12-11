0c0b2dce794fca78470c098805e00397a95ec3af
Adrien <Adalvy.Guedon@gmail.com>
2018-12-11 11:11:21 +0100

    app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
-const clientMongodb = require ('./mongo.connector')
+const clientMongoDB = require ('./mongo.connector')
+
+const usersRouter = require('./users')
+const projectsRouter = require('./projects')
+const vacationRouter = require('./vacation')
+
+clientMongoDB.init()
+.then(client => {
+    app.use('/users', usersRouter)
+    app.use('/projects', projectsRouter)
+    app.use('/vacation', vacationRouter)
+
+    app.get('/', (req, res) => {
+        res.send('TP initiation express')
+    })
+
+    app.listen(9999, () => {
+        console.log('App listening on port 9999')
+    })
+})
+.catch(err => {
+    throw err
+})
